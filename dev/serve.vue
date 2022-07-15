df7969a0d5c6ce1ce83b023e0990d0d9edebd6ff
Syamsoul Azrien <syamsoul.azrien@srglobal.com>
2022-07-15 23:13:44 +0800

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue3DatatableSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  Vue3DatatableSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-datatable-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vue3DatatableSample } from './vue3-datatable-sample.vue';
