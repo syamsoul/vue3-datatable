5d1dd6fa6b103b916f22c192612e9031688379b1
Syamsoul Azrien <syamsoul.azrien@srglobal.com>
2022-07-16 00:02:17 +0800

    test

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Vue3DatatableSample } from './vue3-datatable-sample.vue';
+export { default as SoulTestNew } from './soul-test.vue';
